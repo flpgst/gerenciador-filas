52da4037daa49bcd728f7fed19346623e6cb6167
Filipe Augusto Gonçalves <flpgst@gmail.com>
2021-08-19 01:39:21 -0300

    config mongoose

--- src/server.js
+++ src/server.js
@@ -1,6 +1,12 @@
 import {ApolloServer} from 'apollo-server'
+import mongoose from 'mongoose'
 
 function startServer ({typeDefs, resolvers}) {
+    mongoose.connect('mongodb://localhost:27017/gerenciador-filas', {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+
     const server = new ApolloServer({typeDefs, resolvers})
     server.listen().then(({url}) => console.log(`Server started at ${url}`))
 }
