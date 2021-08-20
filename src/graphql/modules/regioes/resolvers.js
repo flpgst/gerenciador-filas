724b8f5a1d8ef8c16d338b80ae1aacfc48561c78
Filipe Augusto Gonçalves <flpgst@gmail.com>
2021-08-20 00:33:27 -0300

    Regiao schema and resolver

---
+++ src/graphql/modules/regioes/resolvers.js
@@ -0,0 +1,17 @@
+import Regiao from '../../../models/Regiao';
+import Pessoa from '../../../models/Pessoa';
+
+export default {
+    Regiao: {
+        responsavel: (regiao) => Pessoa.findById(regiao.responsavel)
+    },
+    Query: {
+        regioes: () => Regiao.find(),
+        regiao: (_,{id}) => Regiao.findById(id)
+    },
+    Mutation: {
+        createRegiao: (_, { data }) => Regiao.create(data),
+        updateRegiao: (_, { id, data }) => Regiao.findOneAndUpdate(id, data, { new: true }),
+        deleteRegiao: async (_, { id }) => !!(await Regiao.findOneAndDelete(id))
+    }
+}
