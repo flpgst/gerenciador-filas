d265f0c15f657d196c7177437d4b447abcb97c14
Filipe Augusto Gonçalves <flpgst@gmail.com>
2021-08-20 00:32:55 -0300

    Pessoa resolver

--- src/graphql/modules/pessoas/resolvers.js
+++ src/graphql/modules/pessoas/resolvers.js
@@ -1,4 +1,13 @@
+import Pessoa from '../../../models/Pessoa';
+
 export default {
-    Query: {},
-    Mutation: {}
+    Query: {
+        pessoas: () => Pessoa.find(),
+        pessoa: (_,{id}) => Pessoa.findById(id)
+    },
+    Mutation: {
+        createPessoa: (_, { data }) => Pessoa.create(data),
+        updatePessoa: (_, { id, data }) => Pessoa.findOneAndUpdate(id, data, { new: true }),
+        deletePessoa: async (_, { id }) => !!(await Pessoa.findOneAndDelete(id))
+    }
 }
